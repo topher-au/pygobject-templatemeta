568c7272153791078f32929568a44d3b0cd8f2d3
Topher Sheridan <topher@topher.au>
2025-07-15 02:25:04 +1000

    -

--- pygobject_templatemeta/meta.py
+++ pygobject_templatemeta/meta.py
@@ -1,32 +1,45 @@
 from __future__ import annotations
 
+import caseconverter
 import logging
+import os
+import sys
 from functools import partial
+from gi.repository import GLib, GObject, Gio, Gtk
 from typing import Mapping, Sequence
 
-from gi.repository import GLib, GObject, Gio, Gtk
-from gi.types import GObjectMeta
-
-from .template import ClosureElement, MenuElement, ObjectElement, SignalElement, Template
+from .element import BaseElement, ClosureElement, MenuElement, ObjectElement, SignalElement
+from .template import Template
+from .utils import validate_resource_path
 
 logger = logging.getLogger(__name__)
 
+try:
+    from gi.types import GObjectMeta
+except ImportError:
+    # Fallback for older versions of PyGObject that do not have GObjectMeta
+    logger.warning("GObjectMeta not found, using GObject.GObject as fallback.")
+    GObjectMeta = type(GObject.GObject)
 
-# class FUCK(Gtk.Widget):
-#     ...
-#
-# z=FUCK()
-#
-# ...
+
 class TemplateMeta(GObjectMeta, type):
+    """ A metaclass for constructing Gtk Widgets from an XML template definition.
+
+    When defining a new class that inherits from TemplateMeta, you can specify the template using
+    one of the following keyword arguments:
+
+    `template_path`: The path to the XML template file.
+    `template_resource`: The resource name of the XML template file.
+    `template_string`: A string containing the XML template definition.
+    """
     __template__: Template
 
     __template_resource__: str | None = None
     __template_string__: str | None = None
     __template_path__: str | None = None
-    __gtktemplate_methods__: dict[str, SignalElement | ClosureElement] = None
-    __gtktemplate_handlers__: dict[str, SignalElement | ClosureElement] = None
-    __gtktemplate_widgets__: dict[str, ObjectElement] = None
+    __template_methods__: dict[str, SignalElement | ClosureElement] = None
+    __template_handlers__: dict[str, SignalElement | ClosureElement] = None
+    __template_widgets__: dict[str, ObjectElement] = None
 
     def __new__(mcs, name: str, bases: tuple[type], dict_: dict[str, object], **kwargs):
         if '__gtype_name__' not in dict_:
@@ -48,21 +61,50 @@ class TemplateMeta(GObjectMeta, type):
         if hasattr(cls, '__template__'):
             return cls
 
+        template_bytes = None
+
         if template_string := kwargs.pop('template_string', getattr(cls, '__template_string__', None)):
+            # Load the template from a string.
             if isinstance(template_string, str):
                 template_string = template_string.encode('utf-8')
             template_bytes = GLib.Bytes.new(template_string)
 
         elif template_resource := kwargs.pop('template_resource', getattr(cls, '__template_resource__', None)):
+            # Load the template from the resource with the given path.
             validate_resource_path(template_resource)
             template_bytes = Gio.resources_lookup_data(template_resource, Gio.ResourceLookupFlags.NONE)
 
         elif template_path := kwargs.pop('template_path', getattr(cls, '__template_path__', None)):
+            # Load the template from the file with the given path.
             template_file = Gio.File.new_for_path(template_path)
             template_bytes = GLib.Bytes.new(template_file.load_contents()[1])
 
-        else:
-            raise AttributeError('template')
+        if template_bytes is None:
+            # No template data has been loaded yet, so we will try to find it automatically.
+            try:
+                # Get the path to the module file where the class is defined.
+                module = sys.modules.get(cls.__module__)
+                module_path, module_file = os.path.split(module.__file__)
+                module_name, _ = os.path.splitext(module_file)
+
+                converters = (str.lower, caseconverter.snakecase, caseconverter.pascalcase)
+                names = {func(n) for n in (module_name, cls.__name__) for func in converters}
+
+                # Look for possible template files based on the module name.
+                extensions = ('ui', 'xml', 'glade')
+                filenames = [os.path.join(module_path, os.path.extsep.join((name, ext)))
+                             for ext in extensions for name in names]
+
+                for filename in filter(os.path.isfile, filenames):
+                    template_file = Gio.File.new_for_path(filename)
+                    template_bytes = GLib.Bytes.new(template_file.load_contents()[1])
+                    break
+
+                if template_bytes is None:
+                    raise ValueError(f'No template found for {cls.__name__} in {module_path}. ')
+
+            except Exception as e:
+                raise e
 
         template = Template.from_bytes(template_bytes)
         setattr(cls, '__template__', template)
@@ -78,7 +120,8 @@ def register_template(cls: Gtk.Widget):
     bound_methods = {}
     bound_widgets = {}
 
-    def bind_child(element):
+    def bind_child(element: BaseElement):
+        """ Bind a child to the template. """
         element_key = element.name = element.name or element.id_
         if element_key is None:
             logger.debug(f'Could not bind {element.element.tag} on {element.element.sourceline}, no id')
@@ -90,6 +133,7 @@ def register_template(cls: Gtk.Widget):
         cls.bind_template_child_full(element.id_, element.internal, 0)
 
     def bind_callback(element: SignalElement | ClosureElement):
+        """ Bind a callback to the template. """
         method = getattr(element, 'handler' if isinstance(element, SignalElement) else 'function')
 
         if method in bound_methods:
@@ -103,6 +147,7 @@ def register_template(cls: Gtk.Widget):
         bound_methods[method] = element
 
     def bind_menu(element: MenuElement):
+        """ Bind a menu element to the template. """
         if element.id_ is None:
             logger.debug(f'Could not bind {element.element.tag} on {element.element.sourceline}, no id')
             return
@@ -120,30 +165,19 @@ def register_template(cls: Gtk.Widget):
         bind_menu: template.menus.values(),
         bind_child: template.children.values(),
         bind_callback: [*template.signals.values(),
-                        *template.closures.values()]}.items():
+                        *template.closures.values()]
+    }.items():
         for e in element_list:
             bind_func(e)
 
-    # for menu in template.menus.values():
-    #     bind_menu(menu)
-    #
-    # for child in template.children.values():
-    #     bind_child(child)
-    #
-    # for handler in template.signals.values():
-    #     bind_callback(handler)
-    #
-    # for closure in template.closures.values():
-    #     bind_callback(closure)
-
-    cls.__gtktemplate_methods__ = bound_methods
-    cls.__gtktemplate_widgets__ = bound_widgets
+    cls.__template_methods__ = bound_methods
+    cls.__template_widgets__ = bound_widgets
 
     if Gtk._version == '4.0':
-        BuilderScope = define_builder_scope()
-        cls.set_template_scope(BuilderScope())
+        builder_scope = define_builder_scope()
+        cls.set_template_scope(builder_scope())
     else:
-        cls.set_connect_func(connect_func, cls)
+        raise NotImplementedError(f'Gtk {Gtk._version} is unsupported. Please use Gtk 4.0 or later.')
 
     base_init_template = cls.init_template
     cls.__dontuse_ginstance_init__ = lambda s: init_template(s, cls, base_init_template)
@@ -156,19 +190,19 @@ def init_template(self, cls, base_init_template):
     # if self.__class__ is not cls:
     #     raise TypeError(f"Invalid class '{self.__class__.__name__}' for template")
 
-    self.__gtktemplate_handlers__ = set()
+    self.__template_handlers__ = set()
 
     # logger.warning(f'{cls} init_template')
     base_init_template(self)
 
-    for name, element in self.__gtktemplate_widgets__.items():
+    for name, element in self.__template_widgets__.items():
         child = self.get_template_child(cls, element.id_)
         if child is not None:
             # logger.debug(f'Binding {name} to {child}')
             setattr(self, name, child)
 
-    for method, attr in self.__gtktemplate_methods__.items():
-        if method not in self.__gtktemplate_handlers__:
+    for method, attr in self.__template_methods__.items():
+        if method not in self.__template_handlers__:
             logger.warning(f'Missing template handler for method {method}')
             continue
 
@@ -212,30 +246,31 @@ def define_builder_scope():
                               func_name: str,
                               flags: Gtk.BuilderClosureFlags,
                               obj):
-            current_obj: TemplateMeta | GObject.Object = builder.get_current_object() or self._scope_object
+            current_object: TemplateMeta | GObject.Object = (builder.get_current_object()
+                                                             or self._scope_object)
             if not self._scope_object:
-                current_obj = builder.get_current_object()
+                current_object = builder.get_current_object()
 
-                # assert (hasattr(current_obj, '__gtktemplate_methods__')
-                #         and isinstance(current_obj.__gtktemplate_methods__, dict))
-                # assert (hasattr(current_obj, '__gtktemplate_handlers__')
-                #         and isinstance(current_obj.__gtktemplate_handlers__, set))
+                # assert (hasattr(current_obj, '__template_methods__')
+                #         and isinstance(current_obj.__template_methods__, dict))
+                # assert (hasattr(current_obj, '__template_handlers__')
+                #         and isinstance(current_obj.__template_handlers__, set))
 
-                if func_name not in current_obj.__gtktemplate_methods__:
+                if func_name not in current_object.__template_methods__:
                     return
 
-                current_obj.__gtktemplate_handlers__.add(func_name)
-                handler_name = current_obj.__gtktemplate_methods__[func_name]
+                current_object.__template_handlers__.add(func_name)
+                handler_name = current_object.__template_methods__[func_name]
 
             else:
-                current_obj = self._scope_object
+                current_object = self._scope_object
                 handler_name = func_name
                 raise NotImplementedError()
 
             if int(flags & Gtk.BuilderClosureFlags.SWAPPED):
                 raise NotImplementedError('swapped closures are not implemented yet')
 
-            handler, args = extract_handler_and_args(current_obj, handler_name)
+            handler, args = extract_handler_and_args(current_object, handler_name)
 
             p = partial(handler, *args)
             if obj is not None:
@@ -244,15 +279,3 @@ def define_builder_scope():
             return p
 
     return BuilderScope
-
-
-def connect_func(*args, **kwargs):
-    raise NotImplementedError('Gtk3 support is not implemented yet')
-
-
-def validate_resource_path(path: str):
-    try:
-        Gio.resources_get_info(path, Gio.ResourceLookupFlags.NONE)
-
-    except GLib.Error:
-        Gio.resources_lookup_data(path, Gio.ResourceLookupFlags.NONE)
